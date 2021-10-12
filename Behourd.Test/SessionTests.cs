4477cd8f4a7e8e723d1c72d7e04cf3ddef943be7
loanl <loanlemauff@outlook.fr>
2021-10-12 09:45:59 +0200

    create 2 team / 3 team tests

--- Behourd.Test/SessionTests.cs
+++ Behourd.Test/SessionTests.cs
@@ -5,7 +5,7 @@ namespace Behourd.Test
 {
     public class SessionTests
     {
-        [Fact(DisplayName = "Étant donné une session ayant deux joueurs, " +
+        [Fact(DisplayName = "session deux joueurs, " +
                             "quand une partie démarre, " +
                             "alors elle compte deux équipes d'un joueur, chacun différent")]
         public void Two_Players_Make_A_Duel()
@@ -21,11 +21,20 @@ namespace Behourd.Test
             Assert.NotEqual(teams.First().Players.Single(), teams.Last().Players.Single());
         }
 
-        [Fact(DisplayName = "Étant donné une session ayant trois joueurs, " +
+        [Fact(DisplayName = "session trois joueurs, " +
                             "quand une partie démarre, " +
                             "alors elle compte deux équipes équilibrées en poids")]
         public void Three_Players_Make_Two_Wight_Equivalent_Teams()
         {
+            var session = new Session();
+            session.AddPlayers(new Player(), new Player(), new Player());
+
+            var game = session.StartGame();
+            var teams = game.Teams;
+
+            Assert.True(teams.Length == 2);
+            Assert.True(teams.All(team => team.PlayerCount == 1));
+            Assert.NotEqual(teams.First().Players.Single(), teams.Last().Players.Single());
         }
     }
 }
